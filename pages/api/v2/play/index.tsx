e508451abb82a6314233533ca9c95bb7e79a33a2
SeniorDev <tech.eng.me@gmail.com>
2024-05-31 10:16:29 -0400

    I update api

--- pages/api/v2/play/index.tsx
+++ pages/api/v2/play/index.tsx
@@ -1,8 +1,27 @@
 import axios from 'axios';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
 
+const pipelineAsync = promisify(pipeline);
+
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+}
 export default async function handler(req: any, res: any) {
-	const { trackUrl } = req.query;
-	const response = await axios.get(trackUrl);
+  const { trackUrl } = req.query;
 
-	res.status(200).json(response.data);
+  try {
+    const response = await axios.get(trackUrl, { responseType: 'stream' });
+
+    // Set appropriate headers for the response
+    res.setHeader('Content-Type', response.headers['content-type']);
+    res.setHeader('Content-Length', response.headers['content-length']);
+
+    // Stream the data from the axios response to the response object
+    await pipelineAsync(response.data, res);
+  } catch (error) {
+    res.status(500).end('Internal Server Error');
+  }
 }
