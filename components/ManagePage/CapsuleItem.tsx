e4fbb4059aa36ae14fc0809e8d02743ee046f745
SeniorDev <tech.eng.me@gmail.com>
2024-05-31 09:37:36 -0400

    I fix loading stuck bug

--- components/ManagePage/CapsuleItem.tsx
+++ components/ManagePage/CapsuleItem.tsx
@@ -2,25 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { CAPSULE_DROP_ADDRESS, CHAIN_ID } from '@/lib/consts';
 import { Capsule } from '@/lib/types';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const CapsuleItem = ({ capsule }: { capsule: Capsule }) => {
 	const image = `https://capsules-iframe.vercel.app/api/og?tokenId=${encodeURIComponent(capsule?.capsuleId)}&contractAddress=${encodeURIComponent(CAPSULE_DROP_ADDRESS)}&chainId=${encodeURIComponent(CHAIN_ID)}`;
 	const [key, setKey] = useState(0);
-	const [trackedTime, setTrackedTime] = useState(0);
-
-	useEffect(() => {
-		let timeTracker: any;
-		if (trackedTime === -1) return;
-		if (trackedTime >= 10) {
-			setTrackedTime(0);
-			setKey((prev) => prev + 1);
-			return;
-		}
-		timeTracker = setInterval(() => setTrackedTime((prev) => prev + 1), 1000);
-
-		return () => clearInterval(timeTracker);
-	}, [trackedTime]);
 
 	return (
 		<Link href={`/manage/${capsule.capsuleId}`} className="block space-y-4">
@@ -35,7 +21,6 @@ const CapsuleItem = ({ capsule }: { capsule: Capsule }) => {
 							alt="not found image"
 							placeholder="blur"
 							blurDataURL={image}
-							onLoadingComplete={() => setTrackedTime(-1)}
 							onError={() => setKey((prevKey) => prevKey + 1)}
 						/>
 					</div>
