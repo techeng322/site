7e0444fa3b12a64c936387d724645c960b5882a9
SeniorDev <tech.eng.me@gmail.com>
2024-05-31 10:18:18 -0400

    I update backend

--- pages/api/v2/play/index.tsx
+++ pages/api/v2/play/index.tsx
@@ -14,12 +14,8 @@ export default async function handler(req: any, res: any) {
 
   try {
     const response = await axios.get(trackUrl, { responseType: 'stream' });
-
-    // Set appropriate headers for the response
     res.setHeader('Content-Type', response.headers['content-type']);
     res.setHeader('Content-Length', response.headers['content-length']);
-
-    // Stream the data from the axios response to the response object
     await pipelineAsync(response.data, res);
   } catch (error) {
     res.status(500).end('Internal Server Error');
